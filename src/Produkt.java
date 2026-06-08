b3215bbb9231c0ce96fc5b1d00301b52e7915fb1
averagecoder12131 <vatadonaldo@gmail.com>
2026-06-08 14:12:32 +0200

    Es wurde die Klasse Shop (Attribute: eine Liste von Produkte) und Produkt (Attribute: Name-String und Preis-Double) eingefügt, und noch die dazugehörigen Konstruktors.

---
+++ src/Produkt.java
@@ -0,0 +1,10 @@
+public class Produkt {
+
+    private String name;
+    private double preis;
+
+    public Produkt(String name, double preis) {
+        this.name = name;
+        this.preis = preis;
+    }
+}
---
+++ src/Shop.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Shop {
+
+    private ArrayList<Produkt> produkte;
+
+    public Shop() {
+        produkte = new ArrayList<>();
+    }
+}
